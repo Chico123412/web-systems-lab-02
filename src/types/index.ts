37fca99b1a046e3b89d76c1680082fba2afbd691
Chiko123412 <mrsigmawolf3773@gmail.com>
2026-09-20 19:37:33 +0300

    feat: implement library management logic

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,19 @@
 export interface Identifiable {
     readonly id: string;
 }
+
+export interface BookData {
+    id: string;
+    title: string;
+    author: string;
+    publicationYear: number;
+    isBorrowed: boolean;
+    borrowedByUserId: string | null;
+}
+
+export interface UserData {
+    id: string;
+    name: string;
+    email: string;
+    borrowedBookIds: string[];
+}
