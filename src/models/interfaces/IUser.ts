6c0e5fe412b61980655102a9e4e2b2edf98cffe8
Chiko123412 <mrsigmawolf3773@gmail.com>
2026-09-20 19:29:08 +0300

    feat: add domain models and generic library service

---
+++ src/models/interfaces/IUser.ts
@@ -0,0 +1,10 @@
+import type { Identifiable } from "../../types";
+
+export interface IUser extends Identifiable {
+    name: string;
+    email: string;
+    readonly borrowedBookIds: readonly string[];
+
+    addBorrowedBook(bookId: string): void;
+    removeBorrowedBook(bookId: string): void;
+}
